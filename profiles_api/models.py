55a9af1aedf8d5f2cda0453b64aab76fe565f92e
Lucas Tay <tayboonkai@gmail.com>
2023-08-28 21:22:28 +0800

    added superuser creation function

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import BaseUserManager
 
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
+
     def create_user(self, email, name, password=None):
         """Create a new user profile"""
         #check if user has email address input
@@ -22,6 +23,13 @@ class UserProfileManager(BaseUserManager):
 
     def create_superuser(self, email, name, password):
         """Create and save a new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
