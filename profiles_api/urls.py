d902fa0027f5fc1a703b5533dfe0adfeecb17beb
Lucas Tay <tayboonkai@gmail.com>
2023-08-29 16:26:29 +0800

    included router path

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -10,5 +10,6 @@ router.register('hello-viewset', views.HelloViewSet, base_name="hello-viewset")
 
 #when the path search gets here, it will respond with the hello api view
 urlpatterns = [
-    path('hello-view/', views.HelloApiView.as_view())
+    path('hello-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
 ]
