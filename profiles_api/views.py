f8aab4ad3e6cfe52cdc87dc8ed9687d7cd6709b2
Lucas Tay <tayboonkai@gmail.com>
2023-08-30 17:45:18 +0800

    added userprofile serializer class and userprofile view class and the url routing

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework import viewsets
 
 from profiles_api import serializers
+from profiles_api import models
 
 
 # Create your views here.
@@ -105,3 +106,9 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
